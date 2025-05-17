d060a7cdc8315f94ee633ae602cfc26193518772
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2025-05-17 17:38:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -35,46 +35,76 @@ promptArea.oninput = () => {
   submit.disabled = !promptArea.value.trim();
 };
 
-submit.onclick = async () => {
-  let promptValue = promptArea.value;
-
-  if (promptValue.trim()) {
-    wrapper.style.display = "flex";
-    emptyMsg.style.display = "none";
-    // submit.disabled = true;
-    promptArea.value = "";
-    let userMsg = document.createElement("div");
-    userMsg.classList.add("userMsg");
-    userMsg.classList.add("msgBox");
-    userMsg.innerText = promptValue;
-    wrapper.appendChild(userMsg);
-    chatArea.scrollTop = chatArea.scrollHeight;
-    /*
-        <div class="msgBox userMsg">Who are you?</div>
-        <div class="msgBox gptMsg">Hello there! I am CheapGPT</div>
-    */
-    let gptMsg = document.createElement("div");
-    gptMsg.classList.add("gptMsg");
-    gptMsg.classList.add("msgBox");
-    const reply = await getResponse(promptValue);
-    gptMsg.innerHTML = reply;
-    wrapper.appendChild(gptMsg);
-    chatArea.scrollTop = chatArea.scrollHeight;
+window.onkeyup = (e) => {
+  e.preventDefault();
+  if (e.keyCode == 13 && !e.shiftKey) {
+    submit.click();
   }
 };
 
+submit.onclick = async () => {
+  let promptValue = promptArea.value.trim();
+  if (!promptValue) return;
+  wrapper.style.display = "flex";
+  emptyMsg.style.display = "none";
+  submit.disabled = true;
+  submit.innerHTML = "stop_circle";
+  promptArea.value = "";
+  let userMsg = document.createElement("div");
+  userMsg.classList.add("userMsg");
+  userMsg.classList.add("msgBox");
+  userMsg.innerText = promptValue;
+  wrapper.appendChild(userMsg);
+  chatArea.scrollTop = chatArea.scrollHeight;
+  //  <div class="msgBox userMsg">Who are you?</div>
+  //  <div class="msgBox gptMsg">Hello there! I am CheapGPT</div>
+  marked.setOptions({
+    breaks: true,
+  });
+  let gptMsg = document.createElement("div");
+  gptMsg.classList.add("gptMsg");
+  gptMsg.classList.add("msgBox");
+  gptMsg.innerHTML = `<div id="loadBubble">
+  <span></span>
+  <span></span>
+  <span></span>
+  </div>`;
+  wrapper.appendChild(gptMsg);
+  chatArea.scrollTop = chatArea.scrollHeight;
+  let reply = await getResponse(promptValue);
+  gptMsg.textContent = reply;
+
+  renderMathInElement(gptMsg, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "\\[", right: "\\]", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+    ],
+    throwOnError: false,
+  });
+  reply = marked.parse(gptMsg.innerHTML);
+  gptMsg.innerHTML = reply;
+  chatArea.scrollTop = chatArea.scrollHeight;
+  chatsList.innerHTML += `<span class="title">${promptValue.slice(
+    0,
+    15
+  )}</span>`;
+  //<span class="title">chat#1</span>
+};
+
 async function getResponse(promptValue) {
+  const API_KEY = `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`;
   const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
     method: "POST",
     headers: {
-      Authorization:
-        "Bearer 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",
-      "HTTP-Referer": "http://localhost", // Optional. Site URL for rankings on openrouter.ai.
-      "X-Title": "CheapGPT", // Optional. Site title for rankings on openrouter.ai.
+      Authorization: `Bearer ${API_KEY}`,
+      "HTTP-Referer": "http://localhost",
+      "X-Title": "CheapGPT",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "deepseek/deepseek-r1:free",
+      model: "deepseek/deepseek-chat:free",
       messages: [
         {
           role: "user",
@@ -85,5 +115,6 @@ async function getResponse(promptValue) {
   });
   //submit.disabled = false;
   const data = await res.json();
+  submit.innerHTML = "arrow_upward";
   return data.choices?.[0]?.message?.content || "Rate limit exceeded!";
 }
